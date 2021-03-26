1420c686c657e70f3ced9ad8265cead2ef9518f3
linkjavier <linkjavier@hotmail.com>
2021-03-26 00:28:20 -0500

    Deleted unnecesary declaration

--- 0x0D-sorted_array_to_avl/binary_trees.h
+++ 0x0D-sorted_array_to_avl/binary_trees.h
@@ -23,8 +23,6 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
-
-typedef struct binary_tree_s heap_t;
 typedef struct binary_tree_s avl_t;
 
 void binary_tree_print(const binary_tree_t *);
