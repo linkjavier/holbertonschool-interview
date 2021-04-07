e80b5a25fc637ae8fb58613c09eca55cbb64fb02
linkjavier <linkjavier@hotmail.com>
2021-04-06 23:21:14 -0500

    Created an Async Request to call it inside a variable declaration with await. Synchronized Request Done

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,19 +1,33 @@
-#!/usr/bin/env node
+#!/usr/bin/node
 const request = require('request');
 const reqFilm = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2] + '/';
 
-request(reqFilm, (error, response, body) => {
-  if (error) console.error('error:', error);
-  const FilmRequestBody = body;
-  const FilmsObject = JSON.parse(FilmRequestBody);
-  const FilmCharacters = FilmsObject.characters;
+function asyncRequest (url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) reject(error);
+      resolve(body);
+    });
+  });
+}
 
-  for (const k in FilmCharacters) {
-    request(FilmCharacters[k], function (error, response, body) {
-      if (error) console.error('error:', error);
+async function getCharactersOfFilm () {
+  try {
+    const body = await asyncRequest(reqFilm);
+    const FilmRequestBody = body;
+    const FilmsObject = JSON.parse(FilmRequestBody);
+    const FilmCharacters = FilmsObject.characters;
+
+    for (const k in FilmCharacters) {
+      const body = await asyncRequest(FilmCharacters[k]);
       const CharacterRequestBody = body;
       const CharactersObject = JSON.parse(CharacterRequestBody);
       console.log(CharactersObject.name);
-    });
+    }
+  } catch (error) {
+    console.error('ERROR:');
+    console.error(error);
   }
-});
+}
+
+getCharactersOfFilm();
